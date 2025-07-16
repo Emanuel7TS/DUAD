83e304181f6ead682c9614889ad720cdcda214df
Emanuel Hasbun Rojas <143146504+Emanuel7TS@users.noreply.github.com>
2025-07-16 09:30:51 -0600

    Delete week_5 directory

--- week_5/Ejercicio2.py
+++
@@ -1,8 +0,0 @@
-"""
- 2. Cree un programa que itere e imprima un string letra por letra de derecha a izquierda.
-"""
-
-my_string  = "Fullstack Developer"
-
-for i in range(len(my_string) - 1, -1, -1):
-    print(my_string[i])
