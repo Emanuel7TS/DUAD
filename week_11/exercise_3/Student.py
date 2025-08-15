215078754708c71d47f162d45b9c130aeae30c96
Emanuel7TS <hasbun7ts@hotmail.com>
2025-08-15 13:11:19 -0600

    student

--- week_11/exercise_3/Student.py
+++ week_11/exercise_3/Student.py
@@ -1,22 +1,28 @@
-class StudentManager():
-    def __init__(self, students):
-        self.students = []
-     
-    def CalculateAverage(self.students):
-        students_average = []
-        for student in self.students:
-            total = 0
-            average = 0
-            grades = student["grades"].values()
+class Student:
+    def __init__(self, student_id, name, section):
+        self.student_id = student_id
+        self.name = name
+        self.section = section
+        self.grades = {
+            "spanish": None,
+            "english": None,
+            "social_studies": None,
+            "science": None
+        }
 
-            for grade in grades:
-                total += grade
-            average = total/(len(grades))
+    def add_grade(self, subject, grade):
 
-            student_average_info = {"Id":student["id"] ,
-                                    "Name":student["name"],
-                                    "Average":average
-                                    }
-            students_average.append(student_average_info)
+        subject = subject.lower().replace(" ", "_")
 
-        return students_average
+        if subject not in self.grades:
+            raise ValueError(f"Subject '{subject}' is not valid.")
+
+        try:
+            numeric_grade = float(grade)
+        except ValueError:
+            raise ValueError("Grade must be a number.")
+
+        if not 0 <= numeric_grade <= 100:
+            raise ValueError("Grade must be between 0 and 100.")
+
+        self.grades[subject] = numeric_grade
