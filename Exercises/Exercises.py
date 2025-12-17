77a3954e0e1617fec4ec5302caba84f0af42bc99
Emanuel7TS <hasbun7ts@hotmail.com>
2025-12-16 21:34:21 -0600

    week_12

---
+++ Exercises/Exercises.py
@@ -0,0 +1,10 @@
+europe_capitals_by_country = {
+	'spain' : 'madrid',
+	'france' : 'paris',
+	'germany' : 'berlin',
+	'norway' : 'oslo',
+}
+
+for item in europe_capitals_by_country.values():
+    # print(item)
+    print(item)
