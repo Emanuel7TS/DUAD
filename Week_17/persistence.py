1ba8715cce32b23abb5288a75adf9ce44f38dbaa
Emanuel7TS <hasbun7ts@hotmail.com>
2026-05-18 16:23:01 -0600

    semana_17

--- Week_17/persistence.py
+++ Week_17/persistence.py
@@ -12,16 +12,43 @@ def store_categories(categories):
 
         for category_name in categories.keys():
             new_file.write(f"{category_name}\n")
-
     print("\nStudent data was successfully exported to 'students_info.csv'.\n")
 
-def import_valid_csv(students):
-    if os.path.exists('students_info.csv'):
-        with open('students_info.csv', 'r') as csv_file:
-            csv_reader = csv.DictReader(csv_file)
-            for row in csv_reader:
-                new_student = Student.from_dict(row)
-                students.append(new_student)
-                print(new_student)
+
+def store_movements(movements):
+
+    if not movements:
+        raise ValueError("There are no movements to store")
+
+    with open("movements.csv", "w", newline="", encoding="utf-8") as new_file:
+
+        for movement in movements:
+            name = movement.name
+            value = movement.value
+            type = movement.type
+            category = movement.category.name
+            new_file.write(f"{movement.name},{movement.value},{movement.type},{movement.category.name}\n")
+    print("\nStudent data was successfully exported to 'students_info.csv'.\n")
+
+
+def load_categories(manager_cat):
+    if os.path.exists('categories.csv'):
+        with open('categories.csv', 'r') as csv_file:
+            for row in csv_file:
+                manager_cat.add_category(row.strip())
+    else:
+        print("No CSV file found to import.")
+
+
+def load_movements(manager_cat):
+    if os.path.exists('movements.csv'):
+        with open('categories.csv', 'r') as csv_file:
+            for row in csv_file:
+                parts = row.strip().split(",")
+                name = parts[0]
+                value = float(parts[1])
+                type = parts[2]
+                category = parts[3]
+                manager_cat.add_movement(name,value,type,manager_cat.categories[category])
     else:
         print("No CSV file found to import.")
